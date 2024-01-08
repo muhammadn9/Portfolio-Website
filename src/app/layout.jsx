4ad1d6d383a90cfc70e8cd0f9bcbfa9fceb18644
muhammadn9 <20mnaseem@gmail.com>
2024-01-08 16:35:12 -0500

    more changes to icons and layouts

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,9 +2,9 @@ import Favicon from "/public/images/favicon.png";
 import ClientRootLayout from "../components/ClientRootLayout";
 
 export const metadata = {
-  title: "Vaheed Shaik | Software Engineer",
+  title: "Muhammad Naseem | Software Engineer",
   description:
-  "Welcome to Vaheed's Software Engineering Portfolio. Explore a diverse range of projects and see how I can bring your ideas to life. Let's connect and discuss your next venture!",
+    "Welcome to Muhammad's Software Engineering Portfolio. Explore a diverse range of projects and see how I can bring your ideas to life. Let's connect and discuss our next venture!",
   keywords: [
     "portfolio",
     "app",
@@ -12,17 +12,17 @@ export const metadata = {
     "sass",
     "scss",
     "react",
-    "chakra",
-    "hashnode",
     "best portfolio",
-    "best portfolio website"
+    "best portfolio website",
   ],
-    icons: [{
-    rel: 'icon',
-    url: Favicon.src,
-  }]
+  icons: [
+    {
+      rel: "icon",
+      url: Favicon.src,
+    },
+  ],
 };
 
 export default function RootLayout({ children }) {
-  return <ClientRootLayout>{children}</ClientRootLayout>
+  return <ClientRootLayout>{children}</ClientRootLayout>;
 }
