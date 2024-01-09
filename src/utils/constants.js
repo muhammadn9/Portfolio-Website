b9b5ffee72516803656a87c0446d60300cbeec4a
muhammadn9 <20mnaseem@gmail.com>
2024-01-09 16:04:59 -0500

    second attempt at blogs removal

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -51,7 +51,7 @@ export const links = [
   { text: "Skills", path: "/skills" },
   { text: "Projects", path: "/projects" },
   { text: "Experience", path: "/experience" },
-  { text: "Blogs", path: "/blogs" },
+  // { text: "Blogs", path: "/blogs" },
   { text: "Contact", path: "/contact" },
   { text: "Resume", path: "/resume" },
 ];
